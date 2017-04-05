ca53798529111a0aa7023c1440581ddb18c852de
Rich Hildred <rhildred@gmail.com>
2017-04-05 17:19:06 -0400

    initial commit

---
+++ xrdplinux.sh
@@ -0,0 +1,9 @@
+sudo apt-get install chromium-browser
+sudo apt-get install pinta
+sudo apt-get install nodejs npm nodejs-legacy
+sudo apt-get install idle3
+timedatectl set-timezone "America/Toronto"
+sudo add-apt-repository ppa:ys/emojione-picker
+sudo apt-get update
+sudo apt-get install emojione-picker
+mkdir Documents/projects
