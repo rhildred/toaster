406fd43077423165859c29d6c38e68f086a4e933
Richard Hildred <rhildred@gmail.com>
2017-04-06 08:14:56 -0400

    Update xrdplinux.sh

--- xrdplinux.sh
+++ xrdplinux.sh
@@ -1,4 +1,3 @@
-sudo apt-get install 
 sudo curl -o - https://bintray.com/tigervnc/stable/download_file?file_path=ubuntu-16.04LTS%2Famd64%2Ftigervncserver_1.7.1-1ubuntu1_amd64.deb  | dpkg --install
 sudo apt-get -f install
 sudo add-apt-repository ppa:ys/emojione-picker
@@ -16,3 +15,8 @@ sudo apt-get install nodejs npm nodejs-legacy
 sudo apt-get install idle3
 sudo apt-get install emojione-picker
 sudo curl -o - https://go.microsoft.com/fwlink/?LinkID=760868 | dpkg --install
+echo "Type the name of a user to have sudo privileges [ENTER]:"
+read user_name
+sudo adduser $user_name
+sudo usermod -aG sudo $user_name
+sudo -H -u $user_name mkdir ~/Documents/projects
