8438320b30ae997ad0ce11c222af04a5897da97b
Richard Hildred <rhildred@gmail.com>
2017-04-06 09:26:49 -0400

    Update xrdplinux.sh

--- xrdplinux.sh
+++ xrdplinux.sh
@@ -1,4 +1,6 @@
-sudo curl -o - https://bintray.com/tigervnc/stable/download_file?file_path=ubuntu-16.04LTS%2Famd64%2Ftigervncserver_1.7.1-1ubuntu1_amd64.deb  | dpkg --install
+sudo apt-get upgrade
+wget -O tigervnc.deb https://bintray.com/tigervnc/stable/download_file?file_path=ubuntu-16.04LTS%2Famd64%2Ftigervncserver_1.7.1-1ubuntu1_amd64.deb
+sudo dpkg -i tigervnc.deb
 sudo apt-get -f install
 sudo add-apt-repository ppa:ys/emojione-picker
 sudo apt-get update
@@ -14,8 +16,11 @@ sudo apt-get install pinta
 sudo apt-get install nodejs npm nodejs-legacy
 sudo apt-get install idle3
 sudo apt-get install emojione-picker
-sudo curl -o - https://go.microsoft.com/fwlink/?LinkID=760868 | dpkg --install
+wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i vscode.deb
 echo "Type the name of a user to have sudo privileges [ENTER]:"
 read user_name
 sudo adduser $user_name
 sudo usermod -aG sudo $user_name
+sudo su - $user_name -c "mkdir ~/Documents"
+sudo su - $user_name -c "mkdir ~/Documents/projects"
