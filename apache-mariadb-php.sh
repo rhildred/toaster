77d048b8ca853920a0e082b79531d6081a3b7143
Richard Hildred <rhildred@gmail.com>
2017-04-06 15:51:08 -0400

    added libapache2-mpm-itk

--- apache-mariadb-php.sh
+++ apache-mariadb-php.sh
@@ -7,6 +7,7 @@ sudo chown www-data /var/www/html/ -R
 sudo apt-get install mariadb-server mariadb-client
 sudo mysql_secure_installation
 sudo apt-get install php7.0-fpm php7.0-mysql php7.0-common php7.0-gd php7.0-json php7.0-cli php7.0-curl libapache2-mod-php7.0
+sudo apt-get install libapache2-mpm-itk
 sudo a2enmod php7.0
 sudo systemctl restart apache2
 sudo mkdir /etc/apache2/ssl
