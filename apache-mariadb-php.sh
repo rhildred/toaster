a547dc1451858d5589f27b53b635613beb2d79d4
Richard Hildred <rhildred@gmail.com>
2017-04-07 19:56:43 -0400

    Update apache-mariadb-php.sh

--- apache-mariadb-php.sh
+++ apache-mariadb-php.sh
@@ -7,8 +7,7 @@ sudo chown www-data /var/www/html/ -R
 sudo apt-get install mariadb-server mariadb-client
 sudo mysql_secure_installation
 #    need to patch db after
-#    update mysql.user set plugin = 'mysql_native_password' where User='root';
-#    FLUSH PRIVILEGES;
+sudo mysql -u root -e "update mysql.user set plugin = 'mysql_native_password' where User='root'; FLUSH PRIVILEGES;"
 
 sudo apt-get install php7.0-fpm php7.0-mysql php7.0-common php7.0-gd php7.0-json php7.0-cli php7.0-curl libapache2-mod-php7.0
 sudo apt-get install libapache2-mpm-itk
